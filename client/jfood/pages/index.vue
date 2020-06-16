47b27f8043a76ffd1bc0cd6e09765abfb42a7fe4
Herverson de Sousa <herverson.sousa@gmail.com>
2020-06-16 20:54:09 -0300

    Initial client

---
+++ client/jfood/pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <v-list>
+        <v-list-item
+          v-for="(item, i) in products"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name" />
+            R$ {{ item.price }}
+            <v-img
+              :src="`http://localhost:4000${item.image}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+            </v-img>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import axios from '~/plugins/axios'
+
+  export default {
+    async asyncData () {
+      const { data } = await axios.get('/categories/fruits')
+      return { products: data.data }
+    }
+  }
+</script>
