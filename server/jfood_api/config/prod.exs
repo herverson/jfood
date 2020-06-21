460d6d9a0bf2d172ae29eafb14d66add3ca36752
Herverson de Sousa <herverson.sousa@gmail.com>
2020-06-21 18:33:43 -0300

    Init mobile

--- server/jfood_api/config/prod.exs
+++ server/jfood_api/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :jfood_api, JfoodApiWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "https://shimmering-insubstantial-gallowaycow.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
